8028a5f78a36ad5c47c3eebc270d44ccffeb85a1
kadauber <noteflightthemes@gmail.com>
2021-01-09 23:22:12 -0500

    wip add epikrato

--- consts.js
+++ consts.js
@@ -1,15 +1,14 @@
-import * as apokalypsi from './axiom_info/apokalypsi';
-import * as automata from './axiom_info/automata';
+import * as apokalypsi from "./axiom_info/apokalypsi";
+import * as automata from "./axiom_info/automata";
+import * as epikrato from "./axiom_info/epikrato";
 
-export const axioms = [
-  "Apokalypsi",
-  "Automata",
-]
+export const axioms = ["Apokalypsi", "Automata", "Epikrato"];
 
 export const axiomDefs = {
-  "Apokalypsi" : apokalypsi,
-  "Automata" : automata,
-}
+  Apokalypsi: apokalypsi,
+  Automata: automata,
+  Epikrato: epikrato
+};
 
 export const sizeBonuses = {
   0: -2,
@@ -19,4 +18,4 @@ export const sizeBonuses = {
   6: 2,
   12: 3,
   30: 4
-}
+};
--- wondermaker/basic_fields.js
+++ wondermaker/basic_fields.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import SizeSlider from '../size.js';
-import {axioms,axiomDefs} from '../consts';
-import Dots from '../dots.js'
-import {toPlusMinus, getSizeBonus} from '../utils';
+import React, { Component } from "react";
+import SizeSlider from "../size.js";
+import { axioms, axiomDefs } from "../consts";
+import Dots from "../dots.js";
+import { toPlusMinus, getSizeBonus } from "../utils";
 
 export default class BasicFields extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  handleChange = (event,id) => {
+  handleChange = (event, id) => {
     this.props.onValueChange(id, event.target.value);
-  }
+  };
 
   render() {
     const bi = this.props.basicInfo;
@@ -19,45 +19,57 @@ export default class BasicFields extends React.Component {
     const flavor = axiomDefs[bi.axiom].flavors[bi.level][bi.flavor];
     const size = bi.size;
     const sizeBonus = getSizeBonus(size);
-    return <div>
-        <input placeholder="Name" spellCheck="false"
+
+    return (
+      <div>
+        <input
+          placeholder="Name"
+          spellCheck="false"
           value={bi.name}
-          onChange={(e) => this.handleChange(e,"name")}
+          onChange={e => this.handleChange(e, "name")}
         />
-        <textarea placeholder="Short Description"
-          style={{fontSize: '12px'}}
+        <textarea
+          placeholder="Short Description"
+          style={{ fontSize: "12px" }}
           value={bi.description}
-          onChange={(e) => this.handleChange(e,"description")}
+          onChange={e => this.handleChange(e, "description")}
         />
-        <select
-          value={bi.axiom}
-          onChange={(e) => this.handleChange(e,"axiom")}
-        >
-          {axioms.map((val,i) =>
-            <option key={i} value={val}>{val}</option>
-          )}
+        <select value={bi.axiom} onChange={e => this.handleChange(e, "axiom")}>
+          {axioms.map((val, i) => (
+            <option key={i} value={val}>
+              {val}
+            </option>
+          ))}
         </select>
-        <Dots
-          value={bi.level}
-          onChange={(e) => this.handleChange(e,"level")}
-        />
+        <Dots value={bi.level} onChange={e => this.handleChange(e, "level")} />
         <select
           value={bi.flavor}
-          onChange={(e) => this.handleChange(e,"flavor")}
+          onChange={e => this.handleChange(e, "flavor")}
         >
-          {flavors.map((val,i) =>
-            <option key={i} value={val}>{val}</option>
-          )}
-        </select><br/>
-        {("flavorOptions" in flavor) ? flavor.flavorOptions(bi,(e)=>{this.handleChange({target: {value: e}}, "flavorOptions")}) : ""}
-        <div style = {{margin: '8px'}}>
-          Size {size + (size === 30 ? ' or greater' : '' )+ toPlusMinus(sizeBonus, false)}
+          {flavors.map((val, i) => (
+            <option key={i} value={val}>
+              {val}
+            </option>
+          ))}
+        </select>
+        <br />
+        {"flavorOptions" in flavor
+          ? flavor.flavorOptions(bi, e => {
+              this.handleChange({ target: { value: e } }, "flavorOptions");
+            })
+          : ""}
+        <div style={{ margin: "8px" }}>
+          Size{" "}
+          {size +
+            (size === 30 ? " or greater" : "") +
+            toPlusMinus(sizeBonus, false)}
         </div>
         <SizeSlider
-          value = {size}
-          onChange={(e) => this.handleChange({target: {value: e}},"size")}
-          max = {30}
+          value={size}
+          onChange={e => this.handleChange({ target: { value: e } }, "size")}
+          max={30}
         />
-    </div>;
+      </div>
+    );
   }
 }
